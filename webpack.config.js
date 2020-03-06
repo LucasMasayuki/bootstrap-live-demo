8a84e30169fbffeb980d01ec2d32cdc9882ae727
LucasMasayuki <lutamaribuchi@gmail.com>
2020-03-06 19:01:49 -0300

    Mudando path para dist

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
     ],
     output: {
         filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'generated'),
-        publicPath: 'generated',
+        path: path.resolve(__dirname, 'dist'),
+        publicPath: 'dist',
     },
 }
